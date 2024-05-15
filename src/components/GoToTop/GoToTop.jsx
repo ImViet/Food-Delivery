1936971251f2d1ce27e8414568f03443028dcd4a
dinh quoc viet <viet.dinh@aba.com.vn>
2024-05-15 14:01:43 +0700

    Add button go to top

---
+++ src/components/GoToTop/GoToTop.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { assets } from "../../assets/assets";
+import "./GoToTop.css";
+const GoToTop = () => {
+  const [showTopBtn, setShowTopBtn] = useState(false);
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 400) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    });
+  });
+  return (
+    <>
+      {showTopBtn ? (
+        <div className="button-top-container">
+          <img src={assets.arrow_top} alt="" onClick={goToTop} />
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+export default GoToTop;
